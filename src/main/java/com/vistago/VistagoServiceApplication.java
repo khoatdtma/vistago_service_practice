bb9efbedfb79cf40a37d413943e88e4a5ced25f0
leda <Dat.Le@iconplc.com>
2022-03-02 09:32:59 +0700

    init project

---
+++ src/main/java/com/vistago/VistagoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vistago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistagoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VistagoServiceApplication.class, args);
+	}
+
+}
